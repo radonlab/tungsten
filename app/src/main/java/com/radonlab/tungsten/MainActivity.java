a0b732099b91a48fdc0c0d62b1c35a7ceb4732e1
skylerlee <skyler.ac.lee@gmail.com>
2023-05-17 22:58:27 +0800

    feat: add activity

---
+++ app/src/main/java/com/radonlab/tungsten/MainActivity.java
@@ -0,0 +1,16 @@
+package com.radonlab.tungsten;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.radonlab.tungsten.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
