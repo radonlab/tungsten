50b9b67865f067c84e27079d93f1d8212f073a90
skylerlee <skyler.ac.lee@gmail.com>
2023-05-18 23:14:56 +0800

    feat: add window

--- app/src/main/java/com/radonlab/tungsten/ScreenService.java
+++ app/src/main/java/com/radonlab/tungsten/ScreenService.java
@@ -2,10 +2,42 @@ package com.radonlab.tungsten;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class ScreenService extends Service {
+
+    private WindowManager windowManager;
+
+    private Button floatButton;
+
     public ScreenService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        floatButton = new Button(this);
+        floatButton.setText("Click Me!");
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.OPAQUE
+        );
+        layoutParams.gravity = Gravity.CENTER;
+        windowManager.addView(floatButton, layoutParams);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        windowManager.removeView(floatButton);
     }
 
     @Override
