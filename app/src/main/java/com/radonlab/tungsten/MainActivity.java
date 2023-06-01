f4b7310fadf9ee4913964ef47942c1e10de9944f
skylerlee <skyler.ac.lee@gmail.com>
2023-06-01 23:40:04 +0800

    feat: add list

--- app/src/main/java/com/radonlab/tungsten/MainActivity.java
+++ app/src/main/java/com/radonlab/tungsten/MainActivity.java
@@ -4,19 +4,30 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.radonlab.tungsten.dto.ScriptDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list_view);
+        initScriptList();
     }
 
     @Override
@@ -33,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             initScreenService();
         }
+    }
+
+    private void initScriptList() {
+        List<ScriptDTO> dataSource = new ArrayList<>();
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.list_item, dataSource));
     }
 
     private void initScreenService() {
---
+++ app/src/main/java/com/radonlab/tungsten/dto/ScriptDTO.java
@@ -0,0 +1,29 @@
+package com.radonlab.tungsten.dto;
+
+public class ScriptDTO {
+
+    private String name;
+
+    private String content;
+
+    public ScriptDTO(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
